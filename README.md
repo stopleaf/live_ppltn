4d5b902ca5a0d22b23321c0f51d72de17626a679
stopleaf <stopleaf37@gmail.com>
2023-01-25 00:27:47 +0900

    ver 0.0.4 kafka produce, consumer 코드 수정

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
     ver 0.0.1 서울시 인구밀집지역 데이터 수집 테스트 코드 생성
     ver 0.0.2 서울시 실시간 도시 데이터 수집으로 변경 및 Kafka 테스트
     ver 0.0.3 Airflow로 1분 마다 데이터 수집 테스트
+    ver 0.0.4 kafka produce, consumer 코드 수정
--- kafka_producer.py
+++ kafka_producer.py
@@ -1,5 +1,20 @@
 from kafka import KafkaProducer
 from json import dumps
+import requests
+import bs4
+import xmltodict
+
+class Data:
+    def __init__(self):
+        self.open_api_key = '46686d4a4573746f37344553524746'
+
+    def run(self, area_name):
+        url = f'http://openapi.seoul.go.kr:8088/{self.open_api_key}/xml/citydata/1/5/{area_name}'
+        r = requests.get(url)
+        text = r.text
+        xml = bs4.BeautifulSoup(text, 'lxml-xml')
+        xml_to_dict = xmltodict.parse(str(xml))
+        return xml_to_dict
 
 class Producer:
     def __init__(self):
@@ -9,6 +24,22 @@ class Producer:
                                       value_serializer=lambda x: dumps(x).encode('utf-8'),
                                       )
 
-    def producer_send(self, topic, data):
+    def run(self, topic, data):
         self.producer.send(topic, value=data)
         self.producer.flush()
+
+def main():
+    subway_stations = ['가산디지털단지역', '강남역', '건대입구역', '고속터미널역', '교대역',
+                       '구로디지털단지역', '서울역', '선릉역', '신도림역', '신림역',
+                       '신촌·이대역', '왕십리역', '역삼역', '연신내역', '용산역']
+    topic = 'test'
+    data_obj = Data()
+    producer_obj = Producer()
+
+    for i in subway_stations:
+        data = data_obj.run(i)
+        producer_obj.run(topic, data)
+        print(f"{i} 전송 완료")
+
+if __name__ == "__main__":
+    main()
